6cc9ab282af491cdf6cd418457ef1eda27449dae
Bohdan Hushcha <hushchabogdan@gmail.com>
2019-05-13 22:30:19 +0300

    Starting implementation for for translations [en,pl,ru]

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+import { useTranslation } from "react-i18next";
 import {
   Collapse,
   Navbar,
@@ -9,14 +10,14 @@ import {
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem,
-} from 'reactstrap';
-import classes from './Navigation.module.css';
-import { useToggle } from '../../hooks/toggle';
+  DropdownItem
+} from "reactstrap";
+import classes from "./Navigation.module.css";
+import { useToggle } from "../../hooks/toggle";
 
 const Navigation = props => {
-
   const [isOpen, toggle] = useToggle();
+  const { t } = useTranslation();
 
   return (
     <div className={classes.Navigation}>
@@ -28,18 +29,12 @@ const Navigation = props => {
             <NavItem>
               <UncontrolledDropdown nav inNavbar>
                 <DropdownToggle nav caret>
-                  Język
+                  {t("lang")}
                 </DropdownToggle>
                 <DropdownMenu right>
-                  <DropdownItem>
-                    Polski
-                  </DropdownItem>
-                  <DropdownItem>
-                    English
-                  </DropdownItem>
-                  <DropdownItem>
-                    Pусский
-                  </DropdownItem>
+                  <DropdownItem>Polski</DropdownItem>
+                  <DropdownItem>English</DropdownItem>
+                  <DropdownItem>Pусский</DropdownItem>
                 </DropdownMenu>
               </UncontrolledDropdown>
             </NavItem>
